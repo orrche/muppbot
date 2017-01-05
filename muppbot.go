27bbd463351fee94368b3d551003fd2f81d028e1
Henrik Hartelius <henrik@hartelius.org>
2017-01-05 09:12:00 +0000

    Initial commit
    Signed-off-by: Henrik Hartelius <henrik@hartelius.org>

---
+++ muppbot.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+func gettemp() string {
+
+	type Weather struct {
+		Temp string `xml:"value>value"`
+	}
+
+	resp, err := http.Get("http://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station/74460/period/latest-hour/data.xml")
+	if err != nil {
+		log.Print(err)
+	}
+
+	defer resp.Body.Close()
+	xmldata, err := ioutil.ReadAll(resp.Body)
+
+	var v Weather
+	xml.Unmarshal([]byte(xmldata), &v)
+	return v.Temp
+
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Det är ", gettemp(), " grader ute.")
+}
+
+func main() {
+
+	runtime.GOMAXPROCS(1)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		channel := "#muppardev"
+		nick := "mupparbotdev"
+
+		conn, err := net.Dial("tcp", "irc.freenode.org:6667")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Fprintln(conn, "USER", nick, "", nick, "", nick, ":", nick)
+		fmt.Fprintln(conn, "NICK", nick)
+		fmt.Fprintln(conn, "JOIN", channel)
+
+		reader := bufio.NewReader(conn)
+
+		for {
+
+			line, _ := reader.ReadString('\n')
+			data := strings.Split(line, ":")
+
+			cmd := strings.TrimSpace(data[0])
+			val := ""
+			arg := ""
+
+			if len(data) > 1 {
+
+				val = strings.TrimSpace(data[1])
+
+			}
+			if len(data) > 2 {
+				arg = strings.TrimSpace(data[2])
+			}
+			log.Print(cmd, val, arg)
+			if cmd == "PING" {
+				fmt.Fprintln(conn, "PONG", val)
+			}
+
+			if arg == "!mupp" {
+				fmt.Fprintln(conn, "PRIVMSG", channel, ":Muppelimupp!")
+			}
+
+			if arg == "!mupp temp" {
+				fmt.Fprintln(conn, "PRIVMSG", channel, ":I Jönköping (på flygplatsen är det", gettemp(), "grader.")
+			}
+		}
+	}()
+
+	go func() {
+		http.HandleFunc("/", handler)
+		http.ListenAndServe(":8080", nil)
+	}()
+	wg.Wait()
+}
